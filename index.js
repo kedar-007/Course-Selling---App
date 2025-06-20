4e1dda4a23df0a222dfb2d4f2d2350e6d3fe8f78
kedar-007 <cakedar7390@gmail.com>
2025-06-20 10:19:20 +0530

    Routes intialised

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app = express();
+
+
+app.post("/api/v1/user/signup",(req,res)=>{
+    
+})
+
+
+app.post("/api/v1/user/sigin",(req,res)=>{
+
+})
+
+
+app.post("/api/v1/user/purchases",(req,res)=>{
+
+})
+
+app.post("/api/v1/course/purchase",(req,res)=>{
+
+})
+
+app.post("/api/v1/courses",(req,res)=>{
+
+})
+
+
+app.listen(3000)
